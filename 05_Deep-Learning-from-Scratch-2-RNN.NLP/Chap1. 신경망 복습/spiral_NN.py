8b933f54b59a2cf8213e9bcc037a1bdd55f7ae52
Steve-YJ <leeyoungjeon0511@gmail.com>
2020-03-15 22:22:46 +0900

    :tada: Fixed VScode Terminal & Debugging Error

--- 05_Deep-Learning-from-Scratch-2-RNN.NLP/Chap1. 신경망 복습/spiral_NN.py
+++ 05_Deep-Learning-from-Scratch-2-RNN.NLP/Chap1. 신경망 복습/spiral_NN.py
@@ -4,29 +4,22 @@
 
 import os
 import sys
+from matplotlib import pyplot as plt
 
-print('currnet path: :', sys.path[0])
 print('current working directory: ', sys.path[0])
 sys.path.append('..')
-# sys.path.append('c:\\Users\\Lee\\Documents\\steve-home\\05_Deep-Learning-from-Scratch-2-RNN.NLP')
-# sys.path.append('c:\\Users\\Lee\\Documents\\steve-home\\05_Deep-Learning-from-Scratch-2-RNN.NLP\\dataset')
-'''
-for path in sys.path:
-    print(path)
-
-print(len(sys.path))
-'''
-# from dataset import spiral
-# import dataset.spiral as spiral
-# from matplotlib.pyplot import plt
 sys.path.append(r"C:\Users\Lee\Documents\steve-home\05_Deep-Learning-from-Scratch-2-RNN.NLP\dataset")
-import spiral
-# from spiral import *  # from ... import ...구문만 사용하면 Error 발생
-# import dataset.spiral
 
-print('sys.path:', sys.path)
-from matplotlib import pyplot as plt
+# from spiral import *  # from ... import ...구문만 사용하면 Error 발생
+# from ../dataset import spiral # 마찬가지 Error
+# import dataset.spiral as spiral
+from dataset.spiral import *
+
+
+import spiral
 
 x, t = spiral.load_data()
 print('x.shape: ', x.shape)
 print('t.shape: ', t.shape)
+
+print('hello world')
