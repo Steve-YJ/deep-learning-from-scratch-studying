e19d0b20716e3e4fb7ede735c5f86198a45aebc0
Steve-YJ <leeyoungjeon0511@gmail.com>
2020-03-21 22:17:30 +0900

    :bookmark:modifed & ...

--- 05_Deep-Learning-from-Scratch-2-RNN.NLP/common/util.py
+++ 05_Deep-Learning-from-Scratch-2-RNN.NLP/common/util.py
@@ -120,28 +120,28 @@ def create_co_matrix(corpus, vocab_size, window_size=1):
     return co_matrix
 
 
-def ppmi(C, verbose=False, eps = 1e-8):
+def ppmi(C, verbose=False, eps=1e-8):
     '''PPMI(점별 상호정보량) 생성
     :param C: 동시발생 행렬
     :param verbose: 진행 상황을 출력할지 여부
     :return:
     '''
     M = np.zeros_like(C, dtype=np.float32)
-    N = np.sum(C)
-    S = np.sum(C, axis=0)
+    N = np.sum(C)  # 모든 element의 합을 리턴.
+    S = np.sum(C, axis=0)  # axis=0, corpus에서 해당 단어의 총 발생 횟수를 리턴.
     total = C.shape[0] * C.shape[1]
     cnt = 0
 
     for i in range(C.shape[0]):
         for j in range(C.shape[1]):
-            pmi = np.log2(C[i, j] * N / (S[j]*S[i]) + eps)
+            pmi = np.log2(C[i, j]*N / (S[j]S[i] + eps)  # PMI: Pointwise Mutual Information
+
             M[i, j] = max(0, pmi)
 
             if verbose:
                 cnt += 1
                 if cnt % (total//100) == 0:
-                    print('%.1f%% 완료' % (100*cnt/total))
-    return M
+                    print('%.1f%%완료' % (100*cnt/total))
 
 
 def create_contexts_target(corpus, window_size=1):
