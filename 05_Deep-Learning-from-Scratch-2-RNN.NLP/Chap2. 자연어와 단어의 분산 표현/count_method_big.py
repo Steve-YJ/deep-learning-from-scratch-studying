0c04ddea6ab48593b4879d5700e568f87d8e6390
Steve-YJ <leeyoungjeon0511@gmail.com>
2020-03-22 01:53:24 +0900

    :tada: Done!! Congratuation! ||Next: Kakao Crawling & Find Similarity words

---
+++ 05_Deep-Learning-from-Scratch-2-RNN.NLP/Chap2. 자연어와 단어의 분산 표현/count_method_big.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from common.util import most_similar, create_co_matrix, ppmi
+from dataset import ptb
+
+window_size = 2
+wordvec_size = 100
+
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+vocab_size = len(word_to_id)
+print('동시발생 수 계산 ...')
+C = create_co_matrix(corpus, vocab_size, window_size)
+print('PPMI 계산 ...')
+W = ppmi(C, verbose=True)
+
+print('SVD 계산 ...')
+try:
+    # truncated SVD(빠르다!)
+    from sklearn.utis.extmath import randomized_svd
+    U, S, V = randomized_svd(W, n_components=wordvec_size, n_iter=5, random_state=None)
+
+except ImportError:
+    # SVD(느리다)
+    U, S, V = np.linalg.svd(W)
+
+word_vecs = U[:, :wordvec_size]
+
+querys = ['you', 'year', 'car', 'toyota']
+for query in querys:
+    most_similar(query, word_to_id, id_to_word, word_vecs, top=5)
