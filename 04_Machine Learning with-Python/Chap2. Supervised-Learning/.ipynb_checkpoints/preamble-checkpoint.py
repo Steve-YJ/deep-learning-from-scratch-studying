311211b39898d642c3961faf50b206d3a20bfa4c
HesseyInsight <leeyoungjeon0511@gmail.com>
2020-05-16 20:23:46 +0900

    :bookmark: Chap2.Supervised ML ~ KNN Basic

--- 04_Machine Learning with-Python/Chap2. Supervised-Learning/.ipynb_checkpoints/preamble-checkpoint.py
+++ 04_Machine Learning with-Python/Chap2. Supervised-Learning/.ipynb_checkpoints/preamble-checkpoint.py
@@ -5,36 +5,23 @@ import matplotlib.pyplot as plt
 import mglearn
 from cycler import cycler
 
-# matplotlib Font
-from matplotlib import font_manager
-
+# 맷플롯립 한글 폰트 지정
+import matplotlib
+matplotlib.rc('font', family='NanumBarunGothic')
+matplotlib.rcParams['axes.unicode_minus'] = False
 
 set_matplotlib_formats('pdf', 'png')
+plt.rcParams['savefig.dpi'] = 300
 plt.rcParams['figure.dpi'] = 300
 plt.rcParams['image.cmap'] = "viridis"
 plt.rcParams['image.interpolation'] = "none"
 plt.rcParams['savefig.bbox'] = "tight"
 plt.rcParams['lines.linewidth'] = 2
 plt.rcParams['legend.numpoints'] = 1
-
-'''
-font_fname = 'C:/Windows/Fonts/KoPubDotumMedium.ttf'
-font_family = font_manager.FontProperties(fname=font_fname).get_name()
-plt.rcParams["font.family"] = font_family
-plt.rcParams['axes.unicode_minus'] = False
-
-'''
-import matplotlib.pyplot as plt
-from matplotlib import font_manager
-
-font_fname = 'C:/Windows/Fonts/NanumGothic.ttf'
-font_family = font_manager.FontProperties(fname=font_fname).get_name()
-
-plt.rcParams["font.family"] = font_family
-
 plt.rc('axes', prop_cycle=(
     cycler('color', mglearn.plot_helpers.cm_cycle.colors) +
     cycler('linestyle', ['-', '-', "--", (0, (3, 3)), (0, (1.5, 1.5))])))
+
 
 np.set_printoptions(precision=3, suppress=True)
 
@@ -42,3 +29,4 @@ pd.set_option("display.max_columns", 8)
 pd.set_option('precision', 2)
 
 __all__ = ['np', 'mglearn', 'display', 'plt', 'pd']
+© 2020 GitHub, Inc.
--- 04_Machine Learning with-Python/Chap2. Supervised-Learning/preamble.py
+++ 04_Machine Learning with-Python/Chap2. Supervised-Learning/preamble.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 import mglearn
 from cycler import cycler
 
-# matplotlib Font
-from matplotlib import font_manager
-
+# 맷플롯립 한글 폰트 지정
+import matplotlib
+matplotlib.rc('font', family='NanumBarunGothic')
+matplotlib.rcParams['axes.unicode_minus'] = False
 
 set_matplotlib_formats('pdf', 'png')
+plt.rcParams['savefig.dpi'] = 300
 plt.rcParams['figure.dpi'] = 300
 plt.rcParams['image.cmap'] = "viridis"
 plt.rcParams['image.interpolation'] = "none"
@@ -20,9 +22,11 @@ plt.rc('axes', prop_cycle=(
     cycler('color', mglearn.plot_helpers.cm_cycle.colors) +
     cycler('linestyle', ['-', '-', "--", (0, (3, 3)), (0, (1.5, 1.5))])))
 
+
 np.set_printoptions(precision=3, suppress=True)
 
 pd.set_option("display.max_columns", 8)
 pd.set_option('precision', 2)
 
 __all__ = ['np', 'mglearn', 'display', 'plt', 'pd']
+© 2020 GitHub, Inc.
